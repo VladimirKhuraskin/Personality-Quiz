d0ebd107ad7e3fad32bc74910f1f5a90147e714f
Vladimir Khuraskin <35337846+VladimirKhuraskin@users.noreply.github.com>
2018-09-12 10:45:10 +0400

    Initial Commit

---
+++ Personality Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Vladimir Khuraskin on 12.09.2018.
+//  Copyright © 2018 Vladimir Khuraskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
