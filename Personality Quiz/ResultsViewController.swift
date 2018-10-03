b98ac8770b0e5ed5dc2f3a29de0e22d4b30b86d4
Vladimir Khuraskin <khuraskin.dev@gmail.com>
2018-10-03 17:39:05 +0400

    Setup UI

---
+++ Personality Quiz/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  Personality Quiz
+//
+//  Created by Vladimir Khuraskin on 12.09.2018.
+//  Copyright © 2018 Vladimir Khuraskin. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
